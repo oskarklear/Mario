cd272646c77566e738de4df325a2d3fbc7d0b94e
matt-manning <65859640+matt-manning@users.noreply.github.com>
2021-09-25 16:24:43 -0400

    Add project files.

---
+++ Mario/Controller/KeyboardInput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mario
+{
+    class KeyboardInput
+    {
+    }
+}
---
+++ Mario/Sprites/Items/RedMushroom.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mario.Sprites.Items
+{
+    class RedMushroom
+    {
+    }
+}
