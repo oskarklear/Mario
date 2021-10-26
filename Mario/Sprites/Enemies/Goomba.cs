a3a4dc476fef975a6dd62b7dd8baae79465b975e
Oskar Klear <klear.6@buckeyemail.osu.edu>
2021-10-25 21:03:57 -0400

    Fixed goomba sprites

--- Mario/Sprites/Enemies/Goomba.cs
+++ Mario/Sprites/Enemies/Goomba.cs
@@ -46,7 +46,7 @@ namespace Mario.Sprites.Enemies
             position = location;
             Theatre = theatre;
             texture = Theatre.Content.Load<Texture2D>("enemies/goomba/goombaLeft");
-            hitbox = new Rectangle((int)location.X + 13, (int)location.Y + 15, 10, 10);
+            hitbox = new Rectangle((int)location.X, (int)location.Y, 16, 16);
             dead = false;
             showHitbox = false;
         }
