7076d6c46ed037572bc5c059cfaa9fc412347abe
Kyle Shin <shin.765@buckeyemail.osu.edu>
2021-10-26 23:28:27 -0400

    thing moved

--- Mario/Sprites/GoalGate.cs
+++ Mario/Sprites/GoalGate.cs
@@ -12,12 +12,6 @@ namespace Mario.Sprites
     class GoalGate : ISprite
     {
 
-        public bool isShell
-        {
-            get;
-            set;
-        }
-
         Game1 Theatre;
         Texture2D texture;
         Vector2 position;
@@ -71,11 +65,7 @@ namespace Mario.Sprites
         Game1 Theatre;
         Texture2D texture;
         Vector2 position;
-        public bool isShell
-        {
-            get;
-            set;
-        }
+
         public Vector2 Position
         {
             get { return position; }
