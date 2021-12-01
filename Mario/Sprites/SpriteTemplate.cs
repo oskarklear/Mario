1a5e1eb04cefecc5b3949a8bf034ac2a1c871f56
Oskar Klear <klear.6@buckeyemail.osu.edu>
2021-11-30 22:21:18 -0500

    fixed ys ydfef w

--- Mario/Sprites/SpriteTemplate.cs
+++ Mario/Sprites/SpriteTemplate.cs
@@ -148,7 +148,7 @@ namespace Mario.Sprites
             SetHitbox();
             SpawnFromBlock();
             Move();
-            if (position.X < -30 || position.X > 3584)
+            if (position.X < -30 || position.X > 3584 || position.Y > 300)
                 obtained = true;
         }
 
